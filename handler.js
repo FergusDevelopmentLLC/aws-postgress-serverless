6fda5b3db542d9e5cc701d72fb17f93b7f57a6dd
Will Carter <will.carter@fergusllc.com>
2021-02-25 22:05:36 -0700

    add getAllDistricts function

--- handler.js
+++ handler.js
@@ -1,4 +1,5 @@
 'use strict';
+const db = require('./db_connect');
 
 module.exports.hello = async (event) => {
   return {
@@ -15,4 +16,22 @@ module.exports.hello = async (event) => {
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+module.exports.getAllDistricts = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false
+  db.getAll('cb_2018_us_cd116_20m')
+    .then(res => {
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(res)
+      })
+    })
+    .catch(e => {
+      console.log(e);
+      callback(null, {
+        statusCode: e.statusCode || 500,
+        body: 'Error: Could not find districts: ' + e
+      })
+    })
 };
