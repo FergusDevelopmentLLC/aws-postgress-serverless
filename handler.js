c46109943bacac51ad62e0e3253124e62912ef9c
Will Carter <will.carter@fergusllc.com>
2021-03-02 16:10:57 -0700

    refactor, add getGeoJsonSqlFor

--- handler.js
+++ handler.js
@@ -1,47 +1,100 @@
 'use strict';
-const dbConfig = require('./config/db')
+const dbConfig = require('./db')
 const { Client } = require('pg')
 
-const hello = async (event) => {
+const getGeoJsonSqlFor = (sql) => {
+  
+  //remove trailing ; if present
+  if(sql.charAt(sql.length - 1) === ';') sql = sql.substr(0, sql.length - 1)
+  
+  return `SELECT jsonb_build_object(
+            'type', 'FeatureCollection',
+            'features', jsonb_agg(features.feature)
+          )
+          FROM 
+          (
+            SELECT jsonb_build_object(
+            'type', 'Feature',
+            'geometry', ST_AsGeoJSON(geom,3)::jsonb,
+            'properties', to_jsonb(inputs) - 'geom'
+          ) AS feature
+          FROM 
+            (
+              ${sql}
+            ) inputs
+          ) features;`
+}
+
+module.exports.hello = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: 'Hooray! It works!',
         input: event,
       },
       null,
       2
     ),
-  }
+  };
+
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+module.exports.getDistricts = (event, context, callback) => {
+
+  const client = new Client(dbConfig)
+  client.connect()
+
+  let sql = 
+  `SELECT id, geom, statefp, cd116fp, affgeoid, geoid, lsad, cdsessn, aland, awater
+   FROM public.cb_2018_us_cd116_20m
+   limit 1;
+  `.trim()
+
+  sql = getGeoJsonSqlFor(sql)
+
+  client
+    .query(sql, null)
+    .then((res) => {
+      
+      const response = {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": '*',
+          "Access-Control-Allow-Methods": 'GET'
+        },
+        body: JSON.stringify(res.rows[0]['jsonb_build_object']),
+      }
+
+      callback(null, response)
+
+      client.end()
+    })
+    .catch((error) => {
+
+      const errorResponse = {
+        statusCode: error.statusCode || 500,
+        body: `${error}`,
+      }
+
+      callback(null, errorResponse)
+
+      client.end()
+    })
 }
 
-const getDistrictsForState = (event, context, callback) => {
+module.exports.getDistrictsForState = (event, context, callback) => {
 
-  const sql = 
-  `
-  SELECT jsonb_build_object(
-    'type', 'FeatureCollection',
-    'features', jsonb_agg(features.feature)
-  )
-  FROM 
-  (
-    SELECT jsonb_build_object(
-    'type', 'Feature',
-    'geometry', ST_AsGeoJSON(geom,3)::jsonb,
-    'properties', to_jsonb(inputs) - 'geom'
-  ) AS feature
-  FROM 
-    (
-      SELECT districts.*, states.stusps as state_abbrev, states.name as state_name
-      FROM cb_2018_us_state_20m states
-      JOIN cb_2018_us_cd116_20m districts on districts.statefp = states.statefp
-      WHERE stusps = $1
-    ) inputs
-  ) features;
-  `
+  let sql = 
+  `SELECT districts.*, states.stusps as state_abbrev, states.name as state_name
+   FROM cb_2018_us_state_20m states
+   JOIN cb_2018_us_cd116_20m districts on districts.statefp = states.statefp
+   WHERE stusps = $1`.trim()
+
+  sql = getGeoJsonSqlFor(sql)
+
   const client = new Client(dbConfig)
   client.connect()
 
@@ -66,7 +119,7 @@ const getDistrictsForState = (event, context, callback) => {
 
       const errorResponse = {
         statusCode: error.statusCode || 500,
-        body: `Could not find districts : ${error}`,
+        body: `${error}`,
       }
 
       callback(null, errorResponse)
@@ -74,5 +127,3 @@ const getDistrictsForState = (event, context, callback) => {
       client.end()
     })
 }
-
-module.exports = { hello, getDistrictsForState }
